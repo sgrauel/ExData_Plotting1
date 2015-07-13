ab3c3cac7bdfa961a43be6a232c6dfff66eabffc
sgrauel <shawn.m.grauel@gmail.com>
2015-07-13 02:45:30 -0400

    plot 4

---
+++ plot4.R
@@ -0,0 +1,80 @@
+processHPC4 <- function() {
+  
+  library(data.table)
+  
+  # read in the data
+  powerConsump <- fread("./household_power_consumption.txt", sep = ";", header = TRUE, na.strings = c("?"))
+  
+  
+  # save the collumn names
+  
+  attrNames <- names(powerConsump)
+  
+  # give the collumns appropriate types
+  # time zone of Clamart, FR : CET i.e. Central European Time Zone NOTE: date component is irrelevan
+  powerConsump <- powerConsump[,list(Date,
+                                     Time,
+                                     as.double(Global_active_power),
+                                     as.double(Global_reactive_power),
+                                     as.double(Voltage),
+                                     as.double(Global_intensity),
+                                     as.integer(Sub_metering_1),
+                                     as.integer(Sub_metering_2),
+                                     as.integer(Sub_metering_3))]
+  
+  # give back the names
+  setnames(powerConsump,seq(1,length(powerConsump)),attrNames)
+  
+  
+  library(dplyr)
+  
+  # give collumns appropriate names
+  
+  powerConsump <- powerConsump %>% rename(GlobalActivePower = Global_active_power, 
+                                          GlobalReactivePower = Global_reactive_power,
+                                          GlobalIntensity = Global_intensity,
+                                          Submetering1 = Sub_metering_1, Submetering2 = Sub_metering_2, 
+                                          Submetering3 = Sub_metering_3)
+  
+  
+  # filter for observations taken from dates 2007-02-01 or 2007-02-02
+  
+  powerConsump <- powerConsump %>% filter((Date == "1/2/2007") |
+                                            (Date == "2/2/2007"))
+  
+  # combine the date and time collumns into one datetime of type POSIXct, a type with date and time components
+  
+  # zip vectors together
+  powerConsump[,DateTime:=paste(Date,Time,sep = " ")]
+  
+  # mutate datetime to POSIXct
+  powerConsump[,DateTime:=as.POSIXct(DateTime, tz = "CET", format = "%d/%m/%Y %H:%M:%S")]
+  
+  # sort observations by DateTime
+  powerConsump <- powerConsump %>% arrange(DateTime)
+  
+  # select out Date and Time collumns
+  powerConsump <- powerConsump %>% select(3:length(names(powerConsump)))
+  
+  powerConsump
+}
+
+
+makePlot4 <- function(dt) {
+  
+  png("./plot4.png", width=480, height=480)
+  
+  par(mfrow = c(2, 2))
+  plot(dt$DateTime, dt$GlobalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+  plot(dt$DateTime, dt$Voltage, type="l", xlab="datetime", ylab="Voltage")
+  plot(dt$DateTime, dt$Submetering1, type="l", ylab="Energy Submetering", xlab="")
+  lines(dt$DateTime, dt$Submetering2, type="l", col="red")
+  lines(dt$DateTime, dt$Submetering3, type="l", col="blue")
+  legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+  plot(dt$DateTime, dt$GlobalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+  
+  dev.off()
+  
+}
+
+
